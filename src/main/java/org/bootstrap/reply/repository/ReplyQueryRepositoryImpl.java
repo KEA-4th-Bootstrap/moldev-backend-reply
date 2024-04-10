9a85e0fbb7efee128a40bfb392f3cfc9f241c1aa
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-10 18:22:00 +0900

    KBB-7 [Feat] Reply Query dsl repository 구현체 구현

---
+++ src/main/java/org/bootstrap/reply/repository/ReplyQueryRepositoryImpl.java
@@ -0,0 +1,9 @@
+package org.bootstrap.reply.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class ReplyQueryRepositoryImpl implements ReplyQueryRepository{
+}
