bc7c50837cfcbfcd161cac77d3b0d31fcba067f5
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 20:06:14 +0900

    KBB-11 [Feat] 댓글 조회 controller 구현

--- src/main/java/org/bootstrap/reply/controller/ReplyController.java
+++ src/main/java/org/bootstrap/reply/controller/ReplyController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.bootstrap.reply.common.SuccessResponse;
 import org.bootstrap.reply.dto.request.ReplyRequestDto;
 import org.bootstrap.reply.dto.request.ReplyUpdateRequestDto;
+import org.bootstrap.reply.dto.response.ReplyListResponseDto;
 import org.bootstrap.reply.service.ReplyService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,21 +16,29 @@ import org.springframework.web.bind.annotation.*;
 public class ReplyController {
     private final ReplyService replyService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<?>> getReplyList(@PathVariable("id") final Long postId,
+                                                           @RequestParam(required = false) final String parentsId,
+                                                           @RequestParam(required = false) final Pageable pageable) {
+        final ReplyListResponseDto responseDto = replyService.getReplyList(postId, parentsId, pageable);
+        return SuccessResponse.ok(responseDto);
+    }
+
     @PostMapping
-    public ResponseEntity<SuccessResponse<?>> createReply(@RequestBody ReplyRequestDto requestDto) {
+    public ResponseEntity<SuccessResponse<?>> createReply(@RequestBody final ReplyRequestDto requestDto) {
         replyService.createReply(requestDto);
-        return SuccessResponse.ok(null);
+        return SuccessResponse.created(null);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<SuccessResponse<?>> updateReply(@PathVariable("id") String replyId,
-                                                          @RequestBody ReplyUpdateRequestDto requestDto) {
+    public ResponseEntity<SuccessResponse<?>> updateReply(@PathVariable("id") final String replyId,
+                                                          @RequestBody final ReplyUpdateRequestDto requestDto) {
         replyService.updateReply(replyId, requestDto);
         return SuccessResponse.ok(null);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<SuccessResponse<?>> deleteReply(@PathVariable("id") String replyId) {
+    public ResponseEntity<SuccessResponse<?>> deleteReply(@PathVariable("id") final String replyId) {
         replyService.deleteReply(replyId);
         return SuccessResponse.ok(null);
     }
