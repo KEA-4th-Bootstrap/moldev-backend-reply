ffaa80c0dfa39ba28d67e5a3705748cc54b30a7e
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 13:37:15 +0900

    KBB-8 [Feat] reply 생성 mapper 구현

--- src/main/java/org/bootstrap/reply/mapper/ReplyMapper.java
+++ src/main/java/org/bootstrap/reply/mapper/ReplyMapper.java
@@ -1,7 +1,12 @@
 package org.bootstrap.reply.mapper;
 
+import org.bootstrap.reply.dto.request.ReplyRequestDto;
+import org.bootstrap.reply.entity.Reply;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReplyMapper {
+    public Reply toEntity(ReplyRequestDto replyRequestDto) {
+        return Reply.createReply(replyRequestDto);
+    }
 }
