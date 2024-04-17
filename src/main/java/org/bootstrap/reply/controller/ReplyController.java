f49e978482c1842480b81407d090d5939fffd58c
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 18:25:04 +0900

    KBB-10 [Feat] update controller 구현

--- src/main/java/org/bootstrap/reply/controller/ReplyController.java
+++ src/main/java/org/bootstrap/reply/controller/ReplyController.java
@@ -3,12 +3,10 @@ package org.bootstrap.reply.controller;
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.reply.common.SuccessResponse;
 import org.bootstrap.reply.dto.request.ReplyRequestDto;
+import org.bootstrap.reply.dto.request.ReplyUpdateRequestDto;
 import org.bootstrap.reply.service.ReplyService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +17,13 @@ public class ReplyController {
     @PostMapping
     public ResponseEntity<SuccessResponse<?>> createReply(@RequestBody ReplyRequestDto requestDto) {
         replyService.createReply(requestDto);
+        return SuccessResponse.ok(null);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<SuccessResponse<?>> updateReply(@PathVariable("id") String replyId,
+                                                          @RequestBody ReplyUpdateRequestDto requestDto) {
+        replyService.updateReply(replyId, requestDto);
         return SuccessResponse.ok(null);
     }
 }
