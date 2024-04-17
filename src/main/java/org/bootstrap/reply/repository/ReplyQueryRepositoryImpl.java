6a9bd0f836b10c7a472da2100edc67b9bd64e45e
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 13:37:36 +0900

    KBB-8 [Feat] reply 생성 mongo query impl 구현

--- src/main/java/org/bootstrap/reply/repository/ReplyQueryRepositoryImpl.java
+++ src/main/java/org/bootstrap/reply/repository/ReplyQueryRepositoryImpl.java
@@ -1,9 +1,12 @@
 package org.bootstrap.reply.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
 public class ReplyQueryRepositoryImpl implements ReplyQueryRepository{
+    private final MongoTemplate mongoTemplate;
+
 }
