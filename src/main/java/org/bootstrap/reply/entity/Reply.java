7b64d52cb5c5869e6705c6985823c14eda0541e0
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 13:36:41 +0900

    KBB-8 [Feat] reply entity 구현

--- src/main/java/org/bootstrap/reply/entity/Reply.java
+++ src/main/java/org/bootstrap/reply/entity/Reply.java
@@ -2,16 +2,35 @@ package org.bootstrap.reply.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.bootstrap.reply.dto.request.ReplyRequestDto;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
-@Table(name = "reply")
 @Entity
+@Document(collection = "reply")
 public class Reply {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "reply_id")
-    private Long id;
+    @Field(name="_id")
+    private String id;
+    @Field(name="member_id")
+    private Long memberId;
+    @Field(name="post_id")
+    private Long postId;
+    @Field(name="content")
+    private String content;
+    @Field(name="parents_id")
+    private String parentsId;
+
+    public static Reply createReply(ReplyRequestDto replyRequestDto) {
+        return Reply.builder()
+                .memberId(replyRequestDto.memberId())
+                .postId(replyRequestDto.postId())
+                .content(replyRequestDto.content())
+                .parentsId(replyRequestDto.replyId())
+                .build();
+    }
 }
