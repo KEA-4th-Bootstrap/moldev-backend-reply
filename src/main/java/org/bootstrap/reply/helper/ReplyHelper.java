184dd2d7597cba829cb2f2b5ed20ba81c476cf6e
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 15:55:45 +0900

    KBB-8 [Refactor] 오타 수정

--- src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
+++ src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
@@ -2,13 +2,13 @@ package org.bootstrap.reply.helper;
 
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.reply.entity.Reply;
-import org.bootstrap.reply.mongorepository.ReplyMongoMongoRepository;
+import org.bootstrap.reply.mongorepository.ReplyMongoRepository;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class ReplyHelper {
-    private final ReplyMongoMongoRepository replyMongoRepository;
+    private final ReplyMongoRepository replyMongoRepository;
 
     public Reply saveReply(Reply reply) {
         return replyMongoRepository.save(reply);
--- src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoRepository.java
+++ src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoRepository.java
@@ -3,5 +3,5 @@ package org.bootstrap.reply.mongorepository;
 import org.bootstrap.reply.entity.Reply;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ReplyMongoMongoRepository extends MongoRepository<Reply, String>, ReplyMongoQueryRepository {
+public interface ReplyMongoRepository extends MongoRepository<Reply, String>, ReplyMongoQueryRepository {
 }
