f54d4d7840d49e3543b1464e9ceb8ee5f08cb04a
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 13:37:09 +0900

    KBB-8 [Feat] reply 생성 helper 구현

--- src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
+++ src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
@@ -1,6 +1,7 @@
 package org.bootstrap.reply.helper;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.reply.entity.Reply;
 import org.bootstrap.reply.repository.ReplyRepository;
 import org.springframework.stereotype.Component;
 
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReplyHelper {
     private final ReplyRepository replyRepository;
+
+    public Reply saveReply(Reply reply) {
+        return replyRepository.save(reply);
+    }
 }
