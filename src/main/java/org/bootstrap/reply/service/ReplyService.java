4099bc8e2e467260fd50c0b9179918c70e7e5aad
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 13:38:18 +0900

    KBB-8 [Feat] reply service 구현

--- src/main/java/org/bootstrap/reply/service/ReplyService.java
+++ src/main/java/org/bootstrap/reply/service/ReplyService.java
@@ -1,6 +1,8 @@
 package org.bootstrap.reply.service;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.reply.dto.request.ReplyRequestDto;
+import org.bootstrap.reply.entity.Reply;
 import org.bootstrap.reply.helper.ReplyHelper;
 import org.bootstrap.reply.mapper.ReplyMapper;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,13 @@ import org.springframework.stereotype.Service;
 public class ReplyService {
     private final ReplyHelper replyHelper;
     private final ReplyMapper replyMapper;
+
+    public void createReply(ReplyRequestDto requestDto) {
+        Reply reply = createReplyAndSave(requestDto);
+    }
+
+    private Reply createReplyAndSave(ReplyRequestDto requestDto) {
+        Reply reply = replyMapper.toEntity(requestDto);
+        return replyHelper.saveReply(reply);
+    }
 }
