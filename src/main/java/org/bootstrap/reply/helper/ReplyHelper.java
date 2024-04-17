e0c46ab44896a9881314f1c9b6d5a52582a04b09
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 20:07:50 +0900

    KBB-11 [Feat] 댓글 조회 비즈니스로직 구현

--- src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
+++ src/main/java/org/bootstrap/reply/helper/ReplyHelper.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.bootstrap.reply.dto.request.ReplyUpdateRequestDto;
 import org.bootstrap.reply.entity.Reply;
 import org.bootstrap.reply.mongorepository.ReplyMongoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class ReplyHelper {
     private final ReplyMongoRepository replyMongoRepository;
+
+    public Slice<Reply> findReplyList (Long postId, String parentsId, Pageable pageable) {
+        return replyMongoRepository.findReplyDetailVos(postId, parentsId, pageable);
+    }
 
     public void updateReply(String replyId, ReplyUpdateRequestDto replyUpdateRequestDto) {
         replyMongoRepository.updateReplyById(replyId, replyUpdateRequestDto.content());
