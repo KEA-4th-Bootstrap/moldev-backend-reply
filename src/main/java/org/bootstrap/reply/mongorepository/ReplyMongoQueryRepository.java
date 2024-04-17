67cf600dd4be5f9fae1ea1c761102c1240eff04c
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 18:24:33 +0900

    KBB-10 [Feat] update 쿼리 구현

--- src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoQueryRepository.java
+++ src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoQueryRepository.java
@@ -1,4 +1,9 @@
 package org.bootstrap.reply.mongorepository;
 
+import org.bootstrap.reply.entity.Reply;
+
+import java.util.Optional;
+
 public interface ReplyMongoQueryRepository {
+    void updateReplyById(String replyId, String content);
 }
