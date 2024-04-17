14e71a56b18978696af9723021334f47fa977e73
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 20:08:37 +0900

    KBB-11 [Feat] 댓글 조회 Response Dto 구현

--- src/main/java/org/bootstrap/reply/mapper/ReplyMapper.java
+++ src/main/java/org/bootstrap/reply/mapper/ReplyMapper.java
@@ -1,12 +1,18 @@
 package org.bootstrap.reply.mapper;
 
 import org.bootstrap.reply.dto.request.ReplyRequestDto;
+import org.bootstrap.reply.dto.response.ReplyListResponseDto;
 import org.bootstrap.reply.entity.Reply;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReplyMapper {
     public Reply toEntity(ReplyRequestDto replyRequestDto) {
         return Reply.createReply(replyRequestDto);
+    }
+
+    public ReplyListResponseDto toReplyListResponseDto(Slice<Reply> replySlice) {
+        return ReplyListResponseDto.of(replySlice);
     }
 }
