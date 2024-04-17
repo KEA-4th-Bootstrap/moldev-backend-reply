eebdaa1e8d24a544d4355bcc1f240813b4ee0ae3
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-17 20:08:46 +0900

    KBB-11 [Feat] 댓글 조회 query 구현

--- src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoQueryRepository.java
+++ src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoQueryRepository.java
@@ -1,9 +1,11 @@
 package org.bootstrap.reply.mongorepository;
 
 import org.bootstrap.reply.entity.Reply;
-
-import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface ReplyMongoQueryRepository {
+    Slice<Reply> findReplyDetailVos(Long postId, String parentsId, Pageable pageable);
+
     void updateReplyById(String replyId, String content);
 }
