14e507bec94cd009772e3ef7f163cf8eddb20528
OJOJIN <ojy101039@gmail.com>
2024-05-13 19:02:33 +0900

    KBB-90 [Feat] 게시글 내 댓글 개수 확인 쿼리 작성

--- src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoRepository.java
+++ src/main/java/org/bootstrap/reply/mongorepository/ReplyMongoRepository.java
@@ -4,4 +4,6 @@ import org.bootstrap.reply.entity.Reply;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReplyMongoRepository extends MongoRepository<Reply, String>, ReplyMongoQueryRepository {
+
+    Long countByPostIdAndParentsIdIsNull(Long postId);
 }
