d9000396db9fb559f36067e8971c098501587dbe
OJOJIN <ojy101039@gmail.com>
2024-05-22 10:32:43 +0900

    KBB-118 [Fix] 컴포지션에서 사용하는 응답값 수정

--- src/main/java/org/bootstrap/reply/controller/ReplyController.java
+++ src/main/java/org/bootstrap/reply/controller/ReplyController.java
@@ -8,6 +8,7 @@ import org.bootstrap.reply.dto.response.CommentCountResponseDto;
 import org.bootstrap.reply.dto.response.CommentListResponseDto;
 import org.bootstrap.reply.dto.response.ReplyListResponseDto;
 import org.bootstrap.reply.service.ReplyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,23 +20,23 @@ public class ReplyController {
 
     // 게시글 댓글 가져오기
     @GetMapping("/{id}")
-    public ResponseEntity<SuccessResponse<?>> getCommentList(@PathVariable("id") final Long postId) {
+    public ResponseEntity<CommentListResponseDto> getCommentList(@PathVariable("id") final Long postId) {
         final CommentListResponseDto responseDto = replyService.getCommentList(postId);
-        return SuccessResponse.ok(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     // 댓글의 답글 가져오기
     @GetMapping("")
-    public ResponseEntity<SuccessResponse<?>> getReplyList(@RequestParam final String parentsId) {
+    public ResponseEntity<ReplyListResponseDto> getReplyList(@RequestParam final String parentsId) {
         final ReplyListResponseDto responseDto = replyService.getReplyList(parentsId);
-        return SuccessResponse.ok(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     // 게시글에 달린 댓글 수 가져오기
     @GetMapping("/count/{id}")
-    public ResponseEntity<SuccessResponse<?>> getPostCommentCount(@PathVariable("id") final Long postId) {
+    public ResponseEntity<CommentCountResponseDto> getPostCommentCount(@PathVariable("id") final Long postId) {
         final CommentCountResponseDto responseDto = replyService.getPostCommentCount(postId);
-        return SuccessResponse.ok(responseDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PostMapping
